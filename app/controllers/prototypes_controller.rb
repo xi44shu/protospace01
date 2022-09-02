c4d45973d3ef9a9edd20530088ca6b8ba87173aa
joni-deppu <44shuxichuan@gmail.com>
2022-09-02 16:47:43 +0900

    test

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,54 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy], except: [:show, :index]
+
+  def index
+    @prototype = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+    
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+  def update
+    @prototype = Prototype.find(params[:id])
+    if  @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.delete
+    redirect_to root_path
+  end
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
+
+end
